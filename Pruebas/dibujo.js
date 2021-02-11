086020b683e8f2a01982709f7ecc2dc166666025
alucard <faalsaru@gmail.com>
2021-02-11 15:58:57 -0500

    tipos de selectores

--- Pruebas/dibujo.js
+++ Pruebas/dibujo.js
@@ -1,7 +1,17 @@
-alert("Funciona...")
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'rgb(200, 0, 0)';
-ctx.fillRect(10, 10, 50, 50);
+function hola(){
+
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var i = 0;
+  while(true){
+    if(i==10000){
+      ctx.fillStyle = 'rgb(200, 0, 0)';
+      ctx.fillRect(10+(i*10), 10+(i*10), 50, 50);
+      console.log("monda"+i)
+      i=0;
+    }
+    i++;
+  }
+}
